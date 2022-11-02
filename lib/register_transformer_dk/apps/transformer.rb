043fa16c168055932dc36d49eb8b7c24fa56b66f
Josh Williams <josh@spacesnottabs.com>
2022-11-02 20:08:02 +0000

    Updated to use s3 adapter for large messages

--- lib/register_transformer_dk/apps/transformer.rb
+++ lib/register_transformer_dk/apps/transformer.rb
@@ -1,4 +1,5 @@
 require 'register_transformer_dk/config/settings'
+require 'register_transformer_dk/config/adapters'
 require 'register_transformer_dk/bods_mapping/record_processor'
 require 'register_transformer_dk/record_deserializer'
 require 'register_sources_bods/services/publisher'
@@ -11,16 +12,20 @@ $stdout.sync = true
 module RegisterTransformerDk
   module Apps
     class Transformer
-      def initialize(bods_publisher: nil, entity_resolver: nil, bods_mapper: nil, deserializer: nil)
+      def initialize(bods_publisher: nil, entity_resolver: nil, s3_adapter: nil, bods_mapper: nil, deserializer: nil)
         bods_publisher ||= RegisterSourcesBods::Services::Publisher.new
         entity_resolver ||= RegisterSourcesOc::Services::ResolverService.new
+        s3_adapter ||= RegisterTransformerDk::Config::Adapters::S3_ADAPTER
         @bods_mapper = bods_mapper || RegisterTransformerDk::BodsMapping::RecordProcessor.new(
           entity_resolver: entity_resolver,
           bods_publisher: bods_publisher
         )
         @stream_client = RegisterCommon::Services::StreamClientKinesis.new(
           credentials: RegisterTransformerDk::Config::AWS_CREDENTIALS,
-          stream_name: ENV.fetch('DK_STREAM', 'dk_stream')
+          stream_name: ENV.fetch('DK_STREAM', 'dk_stream'),
+          s3_adapter: s3_adapter,
+          s3_prefix: 'large-dk',
+          s3_bucket: ENV['BODS_S3_BUCKET_NAME'],
         )
         @consumer_id = "RegisterTransformerDk"
         @deserializer = RegisterTransformerDk::RecordDeserializer.new
